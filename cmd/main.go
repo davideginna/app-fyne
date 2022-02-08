de6d374ecb19cbfe0ca45caf863f6a5d8cba682b
Davide Ginnasio <d.ginnasio@tdnet.it>
2022-02-08 15:56:38 +0100

    main go

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,45 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
+	// new app
 	a := app.New()
-	w := a.NewWindow("Hello")
-
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
-
+	// new title and window
+	w := a.NewWindow("New Form")
+	// resize window
+	w.Resize(fyne.NewSize(400, 400))
+	// label empty
+	label := widget.NewLabel("")
+	// form widget
+	form := widget.NewForm(
+		// new use form items
+		// 2 arguments
+		// label, widget
+		widget.NewFormItem("Username", widget.NewEntry()),
+		// password
+		widget.NewFormItem("Password", widget.NewPasswordEntry()),
+	)
+	// working on cancel and submit functions of form
+	form.OnCancel = func() {
+		label.Text = "Canceled"
+		label.Refresh()
+	}
+	form.OnSubmit = func() {
+		label.Text = "submitted"
+		label.Refresh()
+	}
+	// we are almost done
+	w.SetContent(
+		container.NewVBox(
+			form,
+			label,
+		),
+	)
 	w.ShowAndRun()
 }
